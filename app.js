16883d36abd66f6db25db902022bac5b7049602e
praveen0767 <praveensrinivasan05@gmail.com>
2026-01-05 23:00:57 +0530

    New

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World!Helllo');
+  res.status(200).send('Hello World! Codetest');
 });
 
 module.exports = app; // 👈 export ONLY the app
